ad72cab072f71b18f8e1f3b32e37daf30d07f5e4
Vitalii <vitovodenko@gmail.com>
2025-05-28 23:20:14 -0400

    Upgrade Chart

--- src/components/Chart/components/index.ts
+++ src/components/Chart/components/index.ts
@@ -1,11 +1,5 @@
-export * from './chart';
-
-export * from './use-chart';
-
 export * from './chart-select';
 
 export * from './chart-legends';
 
 export * from './chart-loading';
-
-export type * from './types';
