0844df0ee117714fc8b62066a4d5ca52fd792fac
Vitalii <vitovodenko@gmail.com>
2025-05-28 23:26:14 -0400

    Upgrade PhoneInput

--- src/components/PhoneInput/types.ts
+++ src/components/PhoneInput/types.ts
@@ -1,16 +1,22 @@
+import type { Theme, SxProps } from '@mui/material/styles';
 import type { TextFieldProps } from '@mui/material/TextField';
-import type { Value, Country } from 'react-phone-number-input/input';
+import type { ButtonBaseProps } from '@mui/material/ButtonBase';
+import type { Props, Value, Country } from 'react-phone-number-input/input';
 
 // ----------------------------------------------------------------------
 
-export type PhoneInputProps = Omit<TextFieldProps, 'onChange' | 'ref'> & {
-  value: string;
-  country?: Country;
-  disableSelect?: boolean;
-  onChange: (newValue: Value) => void;
+export type PhoneInputProps = Props<TextFieldProps> & {
+  hideSelect?: boolean;
 };
 
-export type CountryListProps = {
-  countryCode?: Country;
-  onClickCountry: (inputValue: Country) => void;
+export type PhoneValue = Value;
+export type PhoneCountry = Country;
+
+export type CountryListProps = ButtonBaseProps & {
+  sx?: SxProps<Theme>;
+  searchCountry: string;
+  selectedCountry?: Country;
+  onSearchCountry: (inputValue: string) => void;
+  onSelectedCountry: (inputValue: Country) => void;
+  options: { label: string; code: string; phone: string }[];
 };
