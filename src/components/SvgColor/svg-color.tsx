02df6cab61f09ea1535bcbc7830d75a656a921d4
Vitalii <vitovodenko@gmail.com>
2025-05-28 23:23:12 -0400

    Upgrade SvgColor

--- src/components/SvgColor/svg-color.tsx
+++ src/components/SvgColor/svg-color.tsx
@@ -1,30 +1,35 @@
 import type { SvgColorProps } from './types';
 
-import { forwardRef } from 'react';
+import { mergeClasses } from 'minimal-shared/utils';
 
-import Box from '@mui/material/Box';
+import { styled } from '@mui/material/styles';
 
 import { svgColorClasses } from './classes';
 
 // ----------------------------------------------------------------------
 
-export const SvgColor = forwardRef<HTMLSpanElement, SvgColorProps>(
-  ({ src, className, sx, ...other }, ref) => (
-    <Box
-      ref={ref}
-      component="span"
-      className={svgColorClasses.root.concat(className ? ` ${className}` : '')}
-      sx={{
-        width: 24,
-        height: 24,
-        flexShrink: 0,
-        display: 'inline-flex',
-        bgcolor: 'currentColor',
-        mask: `url(${src}) no-repeat center / contain`,
-        WebkitMask: `url(${src}) no-repeat center / contain`,
-        ...sx,
-      }}
+export function SvgColor({ src, className, sx, ...other }: SvgColorProps) {
+  return (
+    <SvgRoot
+      className={mergeClasses([svgColorClasses.root, className])}
+      sx={[
+        {
+          mask: `url(${src}) no-repeat center / contain`,
+          WebkitMask: `url(${src}) no-repeat center / contain`,
+        },
+        ...(Array.isArray(sx) ? sx : [sx]),
+      ]}
       {...other}
     />
-  )
-);
+  );
+}
+
+// ----------------------------------------------------------------------
+
+const SvgRoot = styled('span')(() => ({
+  width: 24,
+  height: 24,
+  flexShrink: 0,
+  display: 'inline-flex',
+  backgroundColor: 'currentColor',
+}));
