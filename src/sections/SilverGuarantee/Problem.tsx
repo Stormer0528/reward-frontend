c3f1e2f9be2f4653169321aedf3a39146323a2f9
Vitalii <vitovodenko@gmail.com>
2025-06-04 10:56:27 -0400

    Clean up Silver bugs problem section

--- src/sections/SilverGuarantee/Problem.tsx
+++ src/sections/SilverGuarantee/Problem.tsx
@@ -15,34 +15,40 @@ export default function Problem() {
       <Paper sx={{ padding: { lg: '0 120px' } }}>
         <Item container alignItems="center">
           <Grid size={{ md: 2 }}>
-            <ImageWrapper src={`${CONFIG.ASSET_DIR}/assets/images/problem_1.png`} />
+            <Image src={`${CONFIG.ASSET_DIR}/assets/images/problem_1.png`} />
           </Grid>
           <Grid size={{ md: 10 }}>
-            <SubTitle>{`"If you don't hold it, you don't own it…"`}</SubTitle>
+            <SubTitle>&quot;If you don&apos;t hold it, you don&apos;t own it…&quot;</SubTitle>
             <Typography>
-              {`For most in the precious metals community, it's nearly impossible to understand and respect the idea of money that has no physical form. Also, gold and silver have worked as money, holding their value for thousands of years.`}
+              For most in the precious metals community, it&apos;s nearly impossible to understand
+              and respect the idea of money that has no physical form. Also, gold and silver have
+              worked as money, holding their value for thousands of years.
             </Typography>
           </Grid>
         </Item>
         <Item container alignItems="center">
           <Grid size={{ md: 2 }}>
-            <ImageWrapper src={`${CONFIG.ASSET_DIR}/assets/images/problem_2.png`} />
+            <Image src={`${CONFIG.ASSET_DIR}/assets/images/problem_2.png`} />
           </Grid>
           <Grid size={{ md: 10 }}>
             <SubTitle>Stackers got some bad information in 2012.</SubTitle>
             <Typography>
-              {`Long ago, back when it was impossible to know if Bitcoin and crypto were going to stand the tests of time, several industry loudmouths were aggressively anti-bitcoin. Instead of encouraging a diversified and open-minded approach to the emerging technology, “experts” were passionately advising against buying bitcoin. Obviously, if you could go back in time, you'd convince your younger self to act differently.`}
+              Long ago, back when it was impossible to know if Bitcoin and crypto were going to
+              stand the tests of time, several industry loudmouths were aggressively anti-bitcoin.
+              Instead of encouraging a diversified and open-minded approach to the emerging
+              technology, “experts” were passionately advising against buying bitcoin. Obviously, if
+              you could go back in time, you&apos;d convince your younger self to act differently.
             </Typography>
           </Grid>
         </Item>
         <Item container alignItems="center">
           <Grid size={{ md: 2 }}>
-            <ImageWrapper src={`${CONFIG.ASSET_DIR}/assets/images/problem_3.png`} />
+            <Image src={`${CONFIG.ASSET_DIR}/assets/images/problem_3.png`} />
           </Grid>
           <Grid size={{ md: 10 }}>
-            <SubTitle>{`Stackers think it's too late.`}</SubTitle>
+            <SubTitle>Stackers think it&apos;s too late.</SubTitle>
             <Typography>
-              {`Upon missing what could be the greatest investment opportunity of a lifetime, most precious metals collectors have made peace with missing the bitcoin bandwagon and now turn a blind eye to the continued growth of the crypto market. Fortunately, by staying out of crypto, these cautious collectors did manage to avoid plenty of scams and hacks along the way. After all, you can't lose what you don't risk.`}
+              Upon missing what could be the greatest investment opportunity of a lifetime, most precious metals collectors have made peace with missing the bitcoin bandwagon and now turn a blind eye to the continued growth of the crypto market. Fortunately, by staying out of crypto, these cautious collectors did manage to avoid plenty of scams and hacks along the way. After all, you can&apos;t lose what you don&apos;t risk.
             </Typography>
           </Grid>
         </Item>
@@ -55,8 +61,6 @@ const Wrapper = styled(Container)`
   padding: 20px 60px;
   font-family: 'Josefin Sans';
 `;
-
-const ImageWrapper = styled(Image)``;
 
 const Item = styled(Grid)`
   padding: 30px 0;
