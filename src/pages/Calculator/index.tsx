7917323416e3d238a39bec62c180a77d37a7da1a
olek <olek.mob0210@outlook.com>
2025-06-07 23:50:03 -0400

    Update calculator page layout

--- src/pages/Calculator/index.tsx
+++ src/pages/Calculator/index.tsx
@@ -1,5 +1,7 @@
 import { CONFIG } from 'src/config';
 
+import { Breadcrumbs } from 'src/components/Breadcrumbs';
+
 import Calculator from 'src/sections/Calculator';
 
 // ----------------------------------------------------------------------
@@ -7,7 +9,14 @@ import Calculator from 'src/sections/Calculator';
 export default function Page() {
   return (
     <>
-      <title>{`${CONFIG.APP_NAME} - Commission`}</title>
+      <title>{`${CONFIG.APP_NAME} - Calculator`}</title>
+
+      <Breadcrumbs
+        heading="Calculator"
+        sx={{
+          mb: { xs: 1, md: 2 },
+        }}
+      />
 
       <Calculator />
     </>
--- src/sections/Calculator/index.tsx
+++ src/sections/Calculator/index.tsx
@@ -1,23 +1,5 @@
-import { paths } from 'src/routes/paths';
-
-import { DashboardContent } from 'src/layouts/dashboard';
-
-import { Breadcrumbs } from 'src/components/Breadcrumbs';
-
 import CalculatorForm from './Form';
 
 export default function Calculator() {
-  return (
-    <DashboardContent>
-      <Breadcrumbs
-        heading="Calculator"
-        links={[{ name: 'Calculator', href: paths.dashboard.calculator.root }, { name: 'View' }]}
-        sx={{
-          mb: { xs: 1, md: 2 },
-        }}
-      />
-
-      <CalculatorForm />
-    </DashboardContent>
-  );
+  return <CalculatorForm />;
 }
