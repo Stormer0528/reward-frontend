ca404d5d74113857dda7dc597cd7349a2b15ccba
olek <olek.mob0210@outlook.com>
2025-06-08 21:44:08 -0400

    Update file renderer component

--- src/sections/Invoice/List/FileRenderer.tsx
+++ src/sections/Invoice/List/FileRenderer.tsx
@@ -3,19 +3,19 @@ import type { Invoice } from './type';
 
 import { memo } from 'react';
 
-import Stack from '@mui/material/Stack';
-
 import { FileThumbnail } from 'src/components/FileThumbnail';
 
 export const FileRenderer = memo(
   ({ data }: CustomCellRendererProps<Invoice>) => (
-    <Stack direction="row">
-      <FileThumbnail
-        file="png"
-        sx={{ width: 24, cursor: 'pointer' }}
-        onClick={() => window.open(data?.invoiceFile?.url)}
-      />
-    </Stack>
+    <>
+      {data?.invoiceFile && (
+        <FileThumbnail
+          file="png"
+          sx={{ width: 24, cursor: 'pointer' }}
+          onClick={() => window.open(data.invoiceFile?.url)}
+        />
+      )}
+    </>
   ),
   (prev, next) => prev.data?.id === next.data?.id
 );
