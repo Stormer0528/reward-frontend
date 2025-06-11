3f007ad933b97625425493e35507c77f5fa580e1
olek <olek.mob0210@outlook.com>
2025-06-11 09:59:41 -0400

    Update settings schema

--- src/sections/Profile/History/schema.ts
+++ src/sections/Profile/History/schema.ts
@@ -3,5 +3,5 @@ import { z as zod } from 'zod';
 export type SchemaType = zod.infer<typeof Schema>;
 
 export const Schema = zod.object({
-  communication: zod.boolean().default(true),
+  communication: zod.boolean(),
 });
